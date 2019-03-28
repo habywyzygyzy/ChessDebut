5350393e7ffbf2178f409a50ea320a6ca02a0e29
habywyzygyzy <202119@edu.p.lodz.pl>
2019-03-28 12:43:02 +0100

    added scallable gui

--- src/main/java/GUI/StatisticsTablePanel.java
+++ src/main/java/GUI/StatisticsTablePanel.java
@@ -7,14 +7,13 @@ import javax.swing.table.TableModel;
 import java.awt.*;
 
 public class StatisticsTablePanel extends JPanel {
-    public StatisticsTablePanel(int width, int height, String[][] data) {
+    public StatisticsTablePanel(String[][] data) {
         JTable mainTable;
         final String[] columns = {"Next Move", "# of Games", "% of White Victories", "% of Black Victories", "% of Draws"};
         TableModel model = new DefaultTableModel(data, columns);
         mainTable = new JTable(model);
         JTableHeader header = mainTable.getTableHeader();
         this.setLayout(new BorderLayout());
-        this.setPreferredSize(new Dimension(width, height));
         this.add(header, BorderLayout.NORTH);
         this.add(mainTable, BorderLayout.CENTER);
         this.setVisible(true);
