bad632bdccd5728f7f298542563c70320f02d3d9
habywyzygyzy <202119@edu.p.lodz.pl>
2019-09-18 17:58:22 +0200

    updated history singleton

--- src/main/java/singletons/MovesHistorySingleton.java
+++ src/main/java/singletons/MovesHistorySingleton.java
@@ -1,19 +1,13 @@
 package singletons;
 
-import models.Statistics;
-
 import java.util.ArrayList;
 
 public class MovesHistorySingleton {
 
     private static ArrayList<String> moves;
-    private static ArrayList<Integer> rows;
-    private static ArrayList<Integer> columns;
 
-    private MovesHistorySingleton(ArrayList<String> moves, ArrayList<Integer> rows, ArrayList<Integer> columns) {
+    private MovesHistorySingleton(ArrayList<String> moves) {
         MovesHistorySingleton.moves = moves;
-        MovesHistorySingleton.rows = rows;
-        MovesHistorySingleton.columns = columns;
     }
 
     public static MovesHistorySingleton getInstance() {
@@ -21,7 +15,7 @@ public class MovesHistorySingleton {
     }
 
     private static class SingletonHolder {
-        private static final MovesHistorySingleton INSTANCE = new MovesHistorySingleton(moves, rows, columns);
+        private static final MovesHistorySingleton INSTANCE = new MovesHistorySingleton(moves);
     }
 
     public static ArrayList<String> getMoves() {
@@ -30,21 +24,5 @@ public class MovesHistorySingleton {
 
     public static void setMoves(ArrayList<String> moves) {
         MovesHistorySingleton.moves = moves;
-    }
-
-    public static ArrayList<Integer> getRows() {
-        return MovesHistorySingleton.rows;
-    }
-
-    public static void setRows(ArrayList<Integer> rows) {
-        MovesHistorySingleton.rows = rows;
-    }
-
-    public static ArrayList<Integer> getColumns() {
-        return MovesHistorySingleton.columns;
-    }
-
-    public static void setColumns(ArrayList<Integer> columns) {
-        MovesHistorySingleton.columns = columns;
     }
 }
