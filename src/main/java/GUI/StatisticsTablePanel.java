7b427048c1a5124bf23fe3601c43fc7d4be5d748
habywyzygyzy <202119@edu.p.lodz.pl>
2019-09-25 16:37:14 +0200

    fixes

--- src/main/java/GUI/StatisticsTablePanel.java
+++ src/main/java/GUI/StatisticsTablePanel.java
@@ -23,48 +23,6 @@ class StatisticsTablePanel extends JPanel {
         final String[] columns = {"Next Move", "# of Games", "% of White Victories", "% of Black Victories", "% of Draws"};
         final String[][] data = new String[0][];
         final DefaultTableModel model = new DefaultTableModel(data, columns);
-
-        final JButton searchButton = new JButton("Search");
-        searchButton.addActionListener(new ActionListener() {
-            String[] row;
-
-            public void actionPerformed(ActionEvent e) {
-                long start = System.nanoTime();
-                String hit;
-                Double percentDraws, percentWhite, percentBlack;
-                Integer nrOfGames;
-                model.setRowCount(0);
-                for (int i = 0; i < getStats().size(); i++) {
-                    hit = getStats().get(i).getHit();
-                    nrOfGames = getStats().get(i).getWhiteWins() + getStats().get(i).getBlackWins() + getStats().get(i).getDraws();
-                    if (getStats().get(i).getDraws() != 0)
-                        percentDraws = (double) getStats().get(i).getDraws() / nrOfGames * 100.00;
-                    else
-                        percentDraws = 0.00;
-                    percentDraws = BigDecimal.valueOf(percentDraws)
-                            .setScale(2, RoundingMode.HALF_UP)
-                            .doubleValue();
-                    if (getStats().get(i).getWhiteWins() != 0)
-                        percentWhite = (double) getStats().get(i).getWhiteWins() / nrOfGames * 100.00;
-                    else
-                        percentWhite = 0.00;
-                    percentWhite = BigDecimal.valueOf(percentWhite)
-                            .setScale(2, RoundingMode.HALF_UP)
-                            .doubleValue();
-                    if (getStats().get(i).getBlackWins() != 0)
-                        percentBlack = (double) getStats().get(i).getBlackWins() / nrOfGames * 100.00;
-                    else
-                        percentBlack = 0.00;
-                    percentBlack = BigDecimal.valueOf(percentBlack)
-                            .setScale(2, RoundingMode.HALF_UP)
-                            .doubleValue();
-                    row = new String[]{hit, nrOfGames.toString(), percentWhite.toString(), percentBlack.toString(), percentDraws.toString()};
-                    model.addRow(row);
-                }
-                long end = System.nanoTime() - start;
-                System.out.println("Czas potwierdzenia w ms " + end / 1000000);
-            }
-        });
         mainTable = new JTable(model);
         JTableHeader header = mainTable.getTableHeader();
         mainTable.setAutoCreateRowSorter(true);
